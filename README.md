a777280d989e1fb903426c42b785bb1bb91ffc9f
jinxiao <15158133856@163.com>
2023-04-06 14:48:09 +0800

    认识函数式编程

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# functionalParadigm
+# 函数式编程
 learning functional paradigm
+
+
+## 涉及到的知识点
+1. 范畴学
+2. 集合论
+3. lambda运算
+
+
+## 不能被改变的值
+1. Object.freeze();
---
+++ src/demo1.js
@@ -0,0 +1,14 @@
+var add = function (flock_x, flock_y) {
+    return flock_x + flock_y;
+}
+
+var multiply = function (flock_x, flock_y) {
+    return flock_x * flock_y;
+}
+
+var flock_a = 4;
+var flock_b = 2;
+var flock_c = 0;
+
+var result = add(multiply(flock_b, add(flock_a, flock_c)), multiply(flock_a, flock_b));
+console.log(result);
